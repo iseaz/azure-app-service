4d706bd13546f6ff28a31a15fd37dad754c43a4d
Apisit Asavatappitak <apisit@Apisits-MacBook-Pro.local>
2022-08-24 18:04:54 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 app.get('/', (req, res) => {
 	res.send('<h1>LINE Broadcast</h1>')
 })
 
-app.listen(3000, () => console.log('app is running'))
+app.get('/webhook', async (req, res) => {
+	const { data } = await axios.get('https://line-api.nbs.co.th/groups')
+
+	res.json(data)
+})
+
+app.listen(process.env.PORT || 3000, () => console.log('app is running'))
